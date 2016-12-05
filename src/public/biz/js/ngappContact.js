a34145713cec45ed1be3e1e196f89f8f2f881c73
bobenut <33755383@qq.com>
2016-12-05 16:17:50 +0800

    implemennt creatContact dialog used modal of angular-ui-bootstrap lib

--- src/public/biz/js/ngappContact.js
+++ src/public/biz/js/ngappContact.js
@@ -1,5 +1,6 @@
-var app = angular.module('ngappContact', []);
-app.controller('ctrlrContactShow', function($scope, $http) {
+
+var app = angular.module('ngappContact', ['ui.bootstrap']);
+app.controller('ctrlrContactShow', function($scope, $http, $modal, $log) {
     $scope.contactBlocks = {
         a:[],
         b:[],
@@ -35,14 +36,78 @@ app.controller('ctrlrContactShow', function($scope, $http) {
             $scope.contactBlocks[c.nameFirstWordChr].push(c);
         }
     });
+
+    $scope.openCreateDialog = function (size) {
+        var modalInstance = $modal.open({
+            templateUrl: 'contactDialog.html',
+            controller: 'ctrlrContactCreate',
+            size: size
+        });
+
+        modalInstance.result.then(function (ret) {
+            $log.info('Modal ok at: ' + ret);
+        }, function (ret) {
+            $log.info('Modal dismissed at: ' + ret);
+        });
+    };
+
 });
 
-app.controller('ctrlrContactCreate', function($scope, $http) {
+app.controller('ctrlrContactCreate', function($scope, $modalInstance) {
     $scope.contact = {
-        name:''
+        name:'ddd'
+    };
+
+    $scope.dname='kkk';
+
+    $scope.ok = function () {
+        $modalInstance.close('yes kxh');
+    };
+
+    $scope.cancel = function () {
+        $modalInstance.dismiss('cancel kxh');
     };
 });
 
 app.directive('errorMessage',['$compile',function($compile){
+    return {
+        restrict: 'A',
+        require: 'ngModel',
+        link: function(scope, element, attr, ngModel) {
 
+            var parenNode = element.parent();
+            parenNode.addClass("has-feedback");
+
+
+            var subScope = scope.$new(true);
+            //subScope.errorsText={
+            //    required:"此项为必填",
+            //    pattern:scope.title
+            //}
+
+            subScope.hasError=function(){
+                var re=ngModel.$invalid&&ngModel.$dirty;
+                if(re){
+                    parenNode.addClass("has-error");
+                }else{
+                    parenNode.removeClass("has-error");
+                }
+                return re;
+            }
+
+            subScope.errors=function(){
+                return ngModel.$error;
+            }
+
+
+            var errorElement = $compile(`
+                <span   ng-if="hasError()"  class="glyphicon glyphicon-warning-sign form-control-feedback" ></span>
+                <ul class="help-block" ng-if="hasError()">
+                    <li ng-repeat="(error,wrong) in errors()" ng-bind="errorsText[error]">
+                </ul>
+                `)(subScope);
+
+            element.after(errorElement)
+        }
+    };
 }]) ;
