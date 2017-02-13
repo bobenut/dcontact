ec24667904da55de499b682a855331d9c89873cc
bobenut <33755383@qq.com>
2017-02-13 22:58:00 +0800

    commit list contactors function

--- src/public/biz/js/vueContact.js
+++ src/public/biz/js/vueContact.js
@@ -1,60 +1,90 @@
-new Vue({
-    el: '#vuecontact',
+(function(jquery){
+    var vmContactManagement;
+    var vmConctactCreation;
 
-    data:{
-        contactBlocks : {
-            a: [],
-            b: [],
-            c: [],
-            d: [],
-            e: [],
-            f: [],
-            g: [],
-            h: [],
-            i: [],
-            j: [],
-            k: [],
-            l: [],
-            m: [],
-            n: [],
-            o: [],
-            p: [],
-            q: [],
-            r: [],
-            s: [],
-            t: [],
-            u: [],
-            v: [],
-            w: [],
-            x: [],
-            y: [],
-            z: []
-        },
-        contacts : {}
-    },
+    function buildConctactManagementVm(){
+        return new Vue({
+            el: '#vueContactManagement',
 
-    created: function(){
-        this.fetchContacts();
-    },
+            data:{
+                contactBlocks : {
+                    a: [],
+                    b: [],
+                    c: [],
+                    d: [],
+                    e: [],
+                    f: [],
+                    g: [],
+                    h: [],
+                    i: [],
+                    j: [],
+                    k: [],
+                    l: [],
+                    m: [],
+                    n: [],
+                    o: [],
+                    p: [],
+                    q: [],
+                    r: [],
+                    s: [],
+                    t: [],
+                    u: [],
+                    v: [],
+                    w: [],
+                    x: [],
+                    y: [],
+                    z: []
+                },
+                contacts : {}
+            },
 
-    methods:{
-        fetchContacts: function(){
-            var self = this;
-            $.get('/contact/data').done(function(data){
-                console.log('fetch cotnacts result: %s', JSON.stringify(data,null,2));
+            created: function(){
+                this.fetchContacts();
+            },
 
-                if(!data){
-                    return;
+            methods:{
+                fetchContacts: function(){
+                    var self = this;
+                    $.get('/contact/data').done(function(data){
+                        console.log('fetch cotnacts result: %s', JSON.stringify(data,null,2));
+
+                        if(!data){
+                            return;
+                        }                                                                      k
+
+                        for (var i = 0, c; c = data[i++];) {
+                            self.contactBlocks[c.nameFirstWordChr].push(c);
+                            self.contacts[c._id] = c;
+                        }
+                    }).fail(function(err){
+                        console.log('fetch cotnacts error: %s', err);
+                    });
+                },
+                openCreateDialog: function(){
+                    jquery('#vueContactModificationModal').modal('show');
+
+                    jquery('#vueContactModificationModal').on('shown.bs.modal', function () {
+                        vmConctactCreation = buildConctactCreationVm();
+                    });
+
+                    jquery('#vueContactModificationModal').on('hidden.bs.modal', function () {
+                        vmConctactCreation = null;
+                    });
                 }
-
-                for (var i = 0, c; c = data[i++];) {
-                    self.contactBlocks[c.nameFirstWordChr].push(c);
-                    self.contacts[c._id] = c;
-                }
-            }).fail(function(err){
-                console.log('fetch cotnacts error: %s', err);
-            });
-        }
+            }
+        });
     }
-});
+
+    function buildConctactCreationVm(){
+        return new Vue({
+            el:'#vueContactModificationModal',
+            data:{
+                kxh11: '121'
+            }
+        });
+    }
+
+    vmContactManagement   = buildConctactManagementVm();
+})($);
+
 
